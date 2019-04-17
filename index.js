1dc7d17deb91489c3b34aaea54f6b7d7b278ff13
MobileDev418 <oncemoreforyou@outlook.com>
2019-04-17 17:19:55 +0800

    initial project

---
+++ index.js
@@ -0,0 +1,55 @@
+import './public/css/vendor/handsontable/handsontable.full.css';
+import './public/css/vendor/katex/katex.css';
+import './public/css/vendor/share-button/share-button.css';
+
+import 'font-awesome/scss/font-awesome.scss';
+import 'spectrum-colorpicker/spectrum.css';
+import 'perfect-scrollbar/dist/css/perfect-scrollbar.css';
+
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/3024-day.css';
+import 'codemirror/theme/3024-night.css';
+import 'codemirror/theme/ambiance-mobile.css';
+import 'codemirror/theme/ambiance.css';
+import 'codemirror/theme/base16-dark.css';
+import 'codemirror/theme/base16-light.css';
+import 'codemirror/theme/blackboard.css';
+import 'codemirror/theme/cobalt.css';
+import 'codemirror/theme/dracula.css';
+import 'codemirror/theme/eclipse.css';
+import 'codemirror/theme/elegant.css';
+import 'codemirror/theme/erlang-dark.css';
+import 'codemirror/theme/lesser-dark.css';
+import 'codemirror/theme/material.css';
+import 'codemirror/theme/mbo.css';
+import 'codemirror/theme/mdn-like.css';
+import 'codemirror/theme/midnight.css';
+import 'codemirror/theme/monokai.css';
+import 'codemirror/theme/neat.css';
+import 'codemirror/theme/neo.css';
+import 'codemirror/theme/night.css';
+import 'codemirror/theme/paraiso-dark.css';
+import 'codemirror/theme/paraiso-light.css';
+import 'codemirror/theme/pastel-on-dark.css';
+import 'codemirror/theme/rubyblue.css';
+import 'codemirror/theme/seti.css';
+import 'codemirror/theme/solarized.css';
+import 'codemirror/theme/the-matrix.css';
+import 'codemirror/theme/tomorrow-night-eighties.css';
+import 'codemirror/theme/twilight.css';
+import 'codemirror/theme/vibrant-ink.css';
+import 'codemirror/theme/xq-dark.css';
+import 'codemirror/theme/xq-light.css';
+
+import 'nprogress/nprogress.css';
+
+import './styles/main.scss';
+import './styles/colors.scss';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import 'medium-editor/dist/css/medium-editor.css';
+import 'medium-editor/dist/css/themes/mani.css';
+
+//import './src/client-standalone';
+import './src/client';
